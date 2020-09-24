8d170fce2e667db4f75eb1f87bbef21eaee69322
urquha <andy.urquhart@hotmail.co.uk>
2020-09-24 18:32:59 +0100

    Added components and css. Added dice to Tekken

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './MyButton.css'
 
 function MyButton(props) {
     const label = props.label
-    
-    return (
-        <Link to={props.link}>
-            <button style={props.style}>{label}</button>
-        </Link>
-    );
+    if(props.Link){
+        return (
+            <Link to={props.link}>
+                <button className="btn" onClick={props.onClick} style={props.style}>{label}</button>
+            </Link>
+        );
+    }else return (
+        <button className="btn" onClick={props.onClick} style={props.style}>{label}</button>
+    )
+
 }
 
 export default MyButton
--- src/pages/Tekken.jsx
+++ src/pages/Tekken.jsx
@@ -1,27 +1,42 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import MyButton from '../components/MyButton';
+import SplitPane, { Pane } from 'react-split-pane';
+import './Tekken.css';
 
-function Tekken(){
-    
+
+const Tekken = () => {
+    const [blueDice, setBlueDice] = useState("");
+    const [redDice, setRedDice] = useState("")
+
+    const diceRoll = (colour) => {
+        const current_date = new Date();
+        const roll = parseInt(current_date.getMilliseconds()) % 6 + 1;
+        console.log(roll);
+        if(colour === "blue"){
+            setBlueDice(roll);
+        }else if(colour === "red"){
+            setRedDice(roll)
+        }
+        return;
+    };
 
     return (
         <div>
             <h1>Dice Tekken</h1>
-            <MyButton link="/" label="Dont you dare fucking click me you little..."></MyButton>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <p>oi fuck you</p>
-            <p>nah fuck you</p>
-            <MyButton label="Hit Me" style={{backgroundColor:"Blue"}}></MyButton>
-            <MyButton label="Hit Me" style={{backgroundColor:"Red"}}></MyButton>
+            <MyButton link="" label="Dont you dare fucking click me you little..."></MyButton>
+            
+            <div>
+                <SplitPane split='vertical' minSize={50} defaultSize="50%" >
+                        <Pane className="">
+                            <p>{blueDice}</p>
+                            <MyButton label="Hit Me" style={{backgroundColor:"Blue"}} onClick={() => diceRoll("blue")}/>
+                        </Pane>
+                        <Pane className="">
+                            <p>{redDice}</p>
+                            <MyButton label="Hit Me" style={{backgroundColor:"Red"}} onClick={() => diceRoll("red")}/>
+                        </Pane>
+                </SplitPane>
+            </div>
         </div>
     )
 }
